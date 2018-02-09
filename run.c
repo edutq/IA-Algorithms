b83250b54e985f50793a6efee6eef813e44acd31
Edu <etavarezq@gmail.com>
2018-02-09 17:59:47 -0600

    run file for calculator

---
+++ run.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+
+	int res;
+
+	scanf("%i", &num1);
+	scanf("%i", &num2);
+
+	res = num1 + num2;
+
+	printf("%i", res);
+
+
+	return 0;
+
+}
